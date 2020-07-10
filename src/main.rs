9cc57610299464bba339300315c1656d4446ff7a
Jacek Bilski <jacek@bilski.tech>
2020-07-10 21:39:08 +0200

    Listing all queue families and selecting the first one that supports graphics.

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,12 @@ fn main() {
 
     let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
     println!("Got physical device: {:?}", physical);
+
+    for family in physical.queue_families() {
+        println!("Found a queue family with {:?} queue(s)", family.queues_count());
+    }
+    let queue_family = physical.queue_families()
+        .find(|&q| q.supports_graphics())
+        .expect("couldn't find a graphical queue family");
+    println!("Got queue family: {:?}", queue_family);
 }
