1c85fa7fc191e209c3318237c405d1206f6fb04e
Jacek Bilski <jacek@bilski.tech>
2020-07-10 21:24:42 +0200

    Getting the first physical device of all available to the instance.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use vulkano::instance::Instance;
+use vulkano::instance::{Instance, PhysicalDevice};
 use vulkano::instance::InstanceExtensions;
 
 fn main() {
     let instance = Instance::new(None, &InstanceExtensions::none(), None)
         .expect("failed to create instance");
     println!("Got instance: {:?}", instance);
+
+    let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
+    println!("Got physical device: {:?}", physical);
 }
