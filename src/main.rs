c4d4c3c4964e9ffd3b66d76002a7ab5a25e8231d
Jacek Bilski <jacek@bilski.tech>
2020-07-10 21:23:04 +0200

    Start small - get a Vulkan instance with no required extensions.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use vulkano::instance::Instance;
+use vulkano::instance::InstanceExtensions;
+
 fn main() {
-    println!("Hello, world!");
+    let instance = Instance::new(None, &InstanceExtensions::none(), None)
+        .expect("failed to create instance");
+    println!("Got instance: {:?}", instance);
 }
