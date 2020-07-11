484e4d03175d9d5f4052c18151c4238edf6bcc0a
Jacek Bilski <jacek@bilski.tech>
2020-07-11 21:48:47 +0200

    Creating a render pass and a framebuffer. Also, issuing command to enter the render pass.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::sync::Arc;
+
 use image::{ImageBuffer, Rgba};
 use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
 use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer};
 use vulkano::device::{Device, DeviceExtensions, Features};
-use vulkano::format::{ClearValue, Format};
+use vulkano::format::Format;
+use vulkano::framebuffer::Framebuffer;
 use vulkano::image::{Dimensions, StorageImage};
 use vulkano::instance::{Instance, PhysicalDevice};
 use vulkano::instance::InstanceExtensions;
@@ -33,16 +36,38 @@ fn main() {
     let queue = queues.next().unwrap();
     println!("Got a single queue: {:?}", queue);
 
+    let render_pass = Arc::new(vulkano::single_pass_renderpass!(device.clone(),
+        attachments: {
+            color: {
+                load: Clear,
+                store: Store,
+                format: Format::R8G8B8A8Unorm,
+                samples: 1,
+            }
+        },
+        pass: {
+            color: [color],
+            depth_stencil: {}
+        }
+    ).unwrap());
+
     let image = StorageImage::new(device.clone(), Dimensions::Dim2d { width: 1024, height: 1024 },
                                   Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
     println!("Created a Vulkan StorageImage: {:?}", image);
 
+    let framebuffer = Arc::new(Framebuffer::start(render_pass.clone())
+        .add(image.clone()).unwrap()
+        .build().unwrap());
+
     let buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, (0 .. 1024 * 1024 * 4).map(|_| 0u8))
         .expect("failed to create buffer");
 
-    let mut command_builder = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap();
+    let mut command_builder = AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family()).unwrap();
     command_builder
-        .copy_image_to_buffer(image.clone(), buf.clone()).unwrap();
+        .begin_render_pass(framebuffer.clone(), false, vec![[0.015_7, 0., 0.360_7, 1.0].into()])
+        .unwrap()
+        .end_render_pass()
+        .unwrap();
     let command_buffer = command_builder.build().unwrap();
 
     let finished = command_buffer.execute(queue.clone()).unwrap();
