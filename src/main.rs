561ecfea87c87eb0a9db4db5e78795c4f63edbc4
Jacek Bilski <jacek@bilski.tech>
2020-07-11 18:07:47 +0200

    Copy back the image from GPU memory to CPU memory buffer for later processing.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use vulkano::command_buffer::AutoCommandBufferBuilder;
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer};
 use vulkano::device::{Device, DeviceExtensions, Features};
 use vulkano::format::{ClearValue, Format};
 use vulkano::image::{Dimensions, StorageImage};
 use vulkano::instance::{Instance, PhysicalDevice};
 use vulkano::instance::InstanceExtensions;
+use vulkano::sync::GpuFuture;
 
 fn main() {
     let instance = Instance::new(None, &InstanceExtensions::none(), None)
@@ -34,8 +36,17 @@ fn main() {
                                   Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
     println!("Created a Vulkan StorageImage: {:?}", image);
 
+    let buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, (0 .. 1024 * 1024 * 4).map(|_| 0u8))
+        .expect("failed to create buffer");
+
     // clearing the image with "blue" colour
     let mut command_builder = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap();
-    command_builder.clear_color_image(image.clone(), ClearValue::Float([0.0, 0.0, 1.0, 1.0])).unwrap();
+    command_builder
+        .clear_color_image(image.clone(), ClearValue::Float([0.0, 0.0, 1.0, 1.0])).unwrap()
+        .copy_image_to_buffer(image.clone(), buf.clone()).unwrap();
     let command_buffer = command_builder.build().unwrap();
+
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    finished.then_signal_fence_and_flush().unwrap()
+        .wait(None).unwrap();
 }
