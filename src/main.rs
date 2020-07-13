3937c7b16bf6f2635eeb2c788e03e74d9aace732
Jacek Bilski <jacek@bilski.tech>
2020-07-13 09:07:59 +0200

    Trying to cut main() into smaller functions. Seems impossible, because render_pass uses CustomRenderPassDesc struct, that I don't know how to import (if possible at all), it's defined in macro.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use std::sync::Arc;
 use image::{ImageBuffer, Rgba};
 use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
 use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBuffer, DynamicState};
-use vulkano::device::{Device, DeviceExtensions, Features};
+use vulkano::device::{Device, DeviceExtensions, Features, Queue};
 use vulkano::format::Format;
 use vulkano::framebuffer::{Framebuffer, Subpass};
 use vulkano::image::{Dimensions, StorageImage};
 use vulkano::instance::{Instance, InstanceExtensions, PhysicalDevice};
+use vulkano::memory::pool::{PotentialDedicatedAllocation, StdMemoryPoolAlloc};
 use vulkano::pipeline::GraphicsPipeline;
 use vulkano::pipeline::viewport::Viewport;
 use vulkano::sync::GpuFuture;
@@ -28,36 +29,7 @@ struct Vertex {
 vulkano::impl_vertex!(Vertex, position);
 
 fn main() {
-    let instance = Instance::new(None, &InstanceExtensions::none(), None)
-        .expect("failed to create instance");
-    println!("Got instance: {:?}", instance);
-
-    let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
-    println!("Got physical device: {:?}", physical);
-
-    for family in physical.queue_families() {
-        println!("Found a queue family with {:?} queue(s)", family.queues_count());
-    }
-    let queue_family = physical.queue_families()
-        .find(|&q| q.supports_graphics())
-        .expect("couldn't find a graphical queue family");
-    println!("Got queue family: {:?}", queue_family);
-
-    let (device, mut queues) =
-        Device::new(physical, &Features::none(), &DeviceExtensions::none(), [(queue_family, 0.5)].iter().cloned())
-            .expect("failed to create device");
-    println!("Got a device: {:?}", device);
-
-    let queue = queues.next().unwrap();
-    println!("Got a single queue: {:?}", queue);
-
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [ 0.0,  0.5] };
-    let vertex3 = Vertex { position: [ 0.5, -0.25] };
-    let vertex_buffer = CpuAccessibleBuffer::from_iter(
-        device.clone(), BufferUsage::all(), false, vec![vertex1, vertex2, vertex3].into_iter())
-        .unwrap();
-
+    let (device, queue) = init_vulkan();
     let render_pass = Arc::new(vulkano::single_pass_renderpass!(device.clone(),
         attachments: {
             color: {
@@ -72,18 +44,8 @@ fn main() {
             depth_stencil: {}
         }
     ).unwrap());
-
-    let image = StorageImage::new(device.clone(), Dimensions::Dim2d { width: 1024, height: 1024 },
-                                  Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
-    println!("Created a Vulkan StorageImage: {:?}", image);
-
-    let framebuffer = Arc::new(Framebuffer::start(render_pass.clone())
-        .add(image.clone()).unwrap()
-        .build().unwrap());
-
     let vs = vs::Shader::load(device.clone()).expect("failed to create shader module");
     let fs = fs::Shader::load(device.clone()).expect("failed to create shader module");
-
     let pipeline = Arc::new(GraphicsPipeline::start()
         // Defines what kind of vertex input is expected.
         .vertex_input_single_buffer::<Vertex>()
@@ -98,17 +60,27 @@ fn main() {
         // Now that everything is specified, we call `build`.
         .build(device.clone())
         .unwrap());
+    let (render_pass, pipeline) = (render_pass, pipeline);
+    let vertex_buffer = create_vertex_buffer(&device);
 
     let dynamic_state = DynamicState {
         viewports: Some(vec![Viewport {
             origin: [0.0, 0.0],
             dimensions: [1024.0, 1024.0],
-            depth_range: 0.0 .. 1.0,
+            depth_range: 0.0..1.0,
         }]),
-        .. DynamicState::none()
+        ..DynamicState::none()
     };
 
-    let buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, (0 .. 1024 * 1024 * 4).map(|_| 0u8))
+    let image = StorageImage::new(device.clone(), Dimensions::Dim2d { width: 1024, height: 1024 },
+                                  Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
+    println!("Created a Vulkan StorageImage: {:?}", image);
+
+    let framebuffer = Arc::new(Framebuffer::start(render_pass.clone())
+        .add(image.clone()).unwrap()
+        .build().unwrap());
+
+    let buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, (0..1024 * 1024 * 4).map(|_| 0u8))
         .expect("failed to create buffer");
 
     let mut command_builder = AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family()).unwrap();
@@ -131,6 +103,42 @@ fn main() {
         .wait(None).unwrap();
 
     let buffer_content = buf.read().unwrap();
-    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
-    image.save("image.png").unwrap();
+    let result = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    result.save("image.png").unwrap();
+}
+
+fn init_vulkan() -> (Arc<Device>, Arc<Queue>) {
+    let instance = Instance::new(None, &InstanceExtensions::none(), None)
+        .expect("failed to create instance");
+    println!("Got instance: {:?}", instance);
+
+    let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
+    println!("Got physical device: {:?}", physical);
+
+    for family in physical.queue_families() {
+        println!("Found a queue family with {:?} queue(s)", family.queues_count());
+    }
+    let queue_family = physical.queue_families()
+        .find(|&q| q.supports_graphics())
+        .expect("couldn't find a graphical queue family");
+    println!("Got queue family: {:?}", queue_family);
+
+    let (device, mut queues) =
+        Device::new(physical, &Features::none(), &DeviceExtensions::none(), [(queue_family, 0.5)].iter().cloned())
+            .expect("failed to create device");
+    println!("Got a device: {:?}", device);
+
+    let queue = queues.next().unwrap();
+    println!("Got a single queue: {:?}", queue);
+    (device, queue)
+}
+
+fn create_vertex_buffer(device: &Arc<Device>) -> Arc<CpuAccessibleBuffer<[Vertex], PotentialDedicatedAllocation<StdMemoryPoolAlloc>>> {
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [0.0, 0.5] };
+    let vertex3 = Vertex { position: [0.5, -0.25] };
+    let vertex_buffer = CpuAccessibleBuffer::from_iter(
+        device.clone(), BufferUsage::all(), false, vec![vertex1, vertex2, vertex3].into_iter())
+        .unwrap();
+    vertex_buffer
 }
