d6b05ecc181bbe3e3564387621a1c52f96f20296
Jacek Bilski <jacek@bilski.tech>
2020-10-01 20:57:40 +0200

    Let's start with a Window this time.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use winit::event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+
+// settings
+pub const SCR_WIDTH: u32 = 1920;
+pub const SCR_HEIGHT: u32 = 1080;
+
+struct App {}
+
+impl App {
+    fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window {
+        winit::window::WindowBuilder::new()
+            .with_title("Vulkan Christmas Tree")
+            .with_inner_size(winit::dpi::PhysicalSize::new(SCR_WIDTH, SCR_HEIGHT))
+            .build(event_loop)
+            .expect("Failed to create window.")
+    }
+
+    pub fn main_loop(event_loop: EventLoop<()>) {
+        event_loop.run(move |event, _, control_flow| {
+            match event {
+                Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                Event::WindowEvent {
+                    event: WindowEvent::KeyboardInput {
+                        input: KeyboardInput {
+                            virtual_keycode: Some(virtual_code),
+                            state: ElementState::Pressed,
+                            ..
+                        },
+                        ..
+                    },
+                    .. } => {
+                    match virtual_code {
+                        VirtualKeyCode::Escape => {
+                            *control_flow = ControlFlow::Exit;
+                        }
+                        _ => ()
+                    }
+                }
+                _ => ()
+            }
+        });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let _window = App::init_window(&event_loop);
+
+    App::main_loop(event_loop);
 }
