a7722d88adfbe8807979f0bbe5efe03dba9ae21a
Jacek Bilski <jacek@bilski.tech>
2020-11-07 09:04:28 +0100

    Extracting VulkanGraphicsExecution part 4/? - a few functions.

--- src/vulkan.rs
+++ src/vulkan.rs
@@ -947,16 +947,18 @@ struct VulkanGraphicsExecution {
 
 impl VulkanGraphicsExecution {
     fn new(core: &VulkanCore, graphics_setup: &VulkanGraphicsSetup) -> Self {
-        let uniform_buffers =
-            Vulkan::create_uniform_buffers(&core, graphics_setup.swapchain_composite.images.len());
-        let graphics_descriptor_sets = Vulkan::create_graphics_descriptor_sets(
+        let uniform_buffers = VulkanGraphicsExecution::create_uniform_buffers(
+            &core,
+            graphics_setup.swapchain_composite.images.len(),
+        );
+        let graphics_descriptor_sets = VulkanGraphicsExecution::create_graphics_descriptor_sets(
             &core.device,
             graphics_setup.graphics_descriptor_pool,
             graphics_setup.graphics_descriptor_set_layout,
             &uniform_buffers,
             graphics_setup.swapchain_composite.images.len(),
         );
-        let sync_objects = Vulkan::create_sync_objects(&core.device);
+        let sync_objects = VulkanGraphicsExecution::create_sync_objects(&core.device);
 
         VulkanGraphicsExecution {
             clear_value: [0.0, 0.0, 0.0, 0.0],
@@ -973,6 +975,159 @@ impl VulkanGraphicsExecution {
 
             is_framebuffer_resized: false,
         }
+    }
+
+    fn create_uniform_buffers(
+        core: &VulkanCore,
+        swapchain_image_count: usize,
+    ) -> Vec<UniformBuffer> {
+        let mut uniform_buffers = vec![];
+
+        {
+            let buffer_size = std::mem::size_of::<CameraUBO>();
+
+            let mut buffers = vec![];
+            let mut buffers_memory = vec![];
+
+            for _ in 0..swapchain_image_count {
+                let (uniform_buffer, uniform_buffer_memory) = Vulkan::create_buffer(
+                    core,
+                    buffer_size as u64,
+                    vk::BufferUsageFlags::UNIFORM_BUFFER,
+                    vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT,
+                );
+                buffers.push(uniform_buffer);
+                buffers_memory.push(uniform_buffer_memory);
+            }
+
+            uniform_buffers.push(UniformBuffer {
+                buffers,
+                buffers_memory,
+            });
+        }
+        {
+            let buffer_size = std::mem::size_of::<LightsUBO>();
+
+            let mut buffers = vec![];
+            let mut buffers_memory = vec![];
+
+            for _ in 0..swapchain_image_count {
+                let (uniform_buffer, uniform_buffer_memory) = Vulkan::create_buffer(
+                    core,
+                    buffer_size as u64,
+                    vk::BufferUsageFlags::UNIFORM_BUFFER,
+                    vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT,
+                );
+                buffers.push(uniform_buffer);
+                buffers_memory.push(uniform_buffer_memory);
+            }
+
+            uniform_buffers.push(UniformBuffer {
+                buffers,
+                buffers_memory,
+            });
+        }
+        uniform_buffers
+    }
+
+    fn create_graphics_descriptor_sets(
+        device: &ash::Device,
+        descriptor_pool: vk::DescriptorPool,
+        descriptor_set_layout: vk::DescriptorSetLayout,
+        uniforms_buffers: &Vec<UniformBuffer>,
+        swapchain_images_size: usize,
+    ) -> Vec<vk::DescriptorSet> {
+        let mut layouts: Vec<vk::DescriptorSetLayout> = vec![];
+        for _ in 0..swapchain_images_size {
+            layouts.push(descriptor_set_layout);
+        }
+
+        let descriptor_set_allocate_info = vk::DescriptorSetAllocateInfo {
+            descriptor_pool,
+            descriptor_set_count: swapchain_images_size as u32,
+            p_set_layouts: layouts.as_ptr(),
+            ..Default::default()
+        };
+
+        let descriptor_sets = unsafe {
+            device
+                .allocate_descriptor_sets(&descriptor_set_allocate_info)
+                .expect("Failed to allocate descriptor sets!")
+        };
+
+        for (i, &descritptor_set) in descriptor_sets.iter().enumerate() {
+            let descriptor_buffer_info = [
+                vk::DescriptorBufferInfo {
+                    buffer: uniforms_buffers[CAMERA_UBO_INDEX].buffers[i],
+                    offset: 0,
+                    range: std::mem::size_of::<CameraUBO>() as u64,
+                },
+                vk::DescriptorBufferInfo {
+                    buffer: uniforms_buffers[LIGHTS_UBO_INDEX].buffers[i],
+                    offset: 0,
+                    range: std::mem::size_of::<LightsUBO>() as u64,
+                },
+            ];
+
+            let descriptor_write_sets = [vk::WriteDescriptorSet {
+                dst_set: descritptor_set,
+                dst_binding: 0,
+                dst_array_element: 0,
+                descriptor_count: descriptor_buffer_info.len() as u32,
+                descriptor_type: vk::DescriptorType::UNIFORM_BUFFER,
+                p_image_info: ptr::null(),
+                p_buffer_info: descriptor_buffer_info.as_ptr(),
+                p_texel_buffer_view: ptr::null(),
+                ..Default::default()
+            }];
+
+            unsafe {
+                device.update_descriptor_sets(&descriptor_write_sets, &[]);
+            }
+        }
+
+        descriptor_sets
+    }
+
+    fn create_sync_objects(device: &ash::Device) -> SyncObjects {
+        let mut sync_objects = SyncObjects {
+            image_available_semaphores: vec![],
+            render_finished_semaphores: vec![],
+            inflight_fences: vec![],
+        };
+
+        let semaphore_create_info = vk::SemaphoreCreateInfo {
+            ..Default::default()
+        };
+
+        let fence_create_info = vk::FenceCreateInfo {
+            flags: vk::FenceCreateFlags::SIGNALED,
+            ..Default::default()
+        };
+
+        for _ in 0..MAX_FRAMES_IN_FLIGHT {
+            unsafe {
+                let image_available_semaphore = device
+                    .create_semaphore(&semaphore_create_info, None)
+                    .expect("Failed to create Semaphore Object!");
+                let render_finished_semaphore = device
+                    .create_semaphore(&semaphore_create_info, None)
+                    .expect("Failed to create Semaphore Object!");
+                let inflight_fence = device
+                    .create_fence(&fence_create_info, None)
+                    .expect("Failed to create Fence Object!");
+
+                sync_objects
+                    .image_available_semaphores
+                    .push(image_available_semaphore);
+                sync_objects
+                    .render_finished_semaphores
+                    .push(render_finished_semaphore);
+                sync_objects.inflight_fences.push(inflight_fence);
+            }
+        }
+
+        sync_objects
     }
 
     fn drop(&mut self, device: &ash::Device) {
@@ -1610,59 +1765,6 @@ impl Vulkan {
         }
     }
 
-    fn create_uniform_buffers(
-        core: &VulkanCore,
-        swapchain_image_count: usize,
-    ) -> Vec<UniformBuffer> {
-        let mut uniform_buffers = vec![];
-
-        {
-            let buffer_size = std::mem::size_of::<CameraUBO>();
-
-            let mut buffers = vec![];
-            let mut buffers_memory = vec![];
-
-            for _ in 0..swapchain_image_count {
-                let (uniform_buffer, uniform_buffer_memory) = Vulkan::create_buffer(
-                    core,
-                    buffer_size as u64,
-                    vk::BufferUsageFlags::UNIFORM_BUFFER,
-                    vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT,
-                );
-                buffers.push(uniform_buffer);
-                buffers_memory.push(uniform_buffer_memory);
-            }
-
-            uniform_buffers.push(UniformBuffer {
-                buffers,
-                buffers_memory,
-            });
-        }
-        {
-            let buffer_size = std::mem::size_of::<LightsUBO>();
-
-            let mut buffers = vec![];
-            let mut buffers_memory = vec![];
-
-            for _ in 0..swapchain_image_count {
-                let (uniform_buffer, uniform_buffer_memory) = Vulkan::create_buffer(
-                    core,
-                    buffer_size as u64,
-                    vk::BufferUsageFlags::UNIFORM_BUFFER,
-                    vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT,
-                );
-                buffers.push(uniform_buffer);
-                buffers_memory.push(uniform_buffer_memory);
-            }
-
-            uniform_buffers.push(UniformBuffer {
-                buffers,
-                buffers_memory,
-            });
-        }
-        uniform_buffers
-    }
-
     fn create_graphics_descriptor_pool(
         device: &ash::Device,
         swapchain_images_size: usize,
@@ -1692,65 +1794,6 @@ impl Vulkan {
                 .create_descriptor_pool(&descriptor_pool_create_info, None)
                 .expect("Failed to create Descriptor Pool!")
         }
-    }
-
-    fn create_graphics_descriptor_sets(
-        device: &ash::Device,
-        descriptor_pool: vk::DescriptorPool,
-        descriptor_set_layout: vk::DescriptorSetLayout,
-        uniforms_buffers: &Vec<UniformBuffer>,
-        swapchain_images_size: usize,
-    ) -> Vec<vk::DescriptorSet> {
-        let mut layouts: Vec<vk::DescriptorSetLayout> = vec![];
-        for _ in 0..swapchain_images_size {
-            layouts.push(descriptor_set_layout);
-        }
-
-        let descriptor_set_allocate_info = vk::DescriptorSetAllocateInfo {
-            descriptor_pool,
-            descriptor_set_count: swapchain_images_size as u32,
-            p_set_layouts: layouts.as_ptr(),
-            ..Default::default()
-        };
-
-        let descriptor_sets = unsafe {
-            device
-                .allocate_descriptor_sets(&descriptor_set_allocate_info)
-                .expect("Failed to allocate descriptor sets!")
-        };
-
-        for (i, &descritptor_set) in descriptor_sets.iter().enumerate() {
-            let descriptor_buffer_info = [
-                vk::DescriptorBufferInfo {
-                    buffer: uniforms_buffers[CAMERA_UBO_INDEX].buffers[i],
-                    offset: 0,
-                    range: std::mem::size_of::<CameraUBO>() as u64,
-                },
-                vk::DescriptorBufferInfo {
-                    buffer: uniforms_buffers[LIGHTS_UBO_INDEX].buffers[i],
-                    offset: 0,
-                    range: std::mem::size_of::<LightsUBO>() as u64,
-                },
-            ];
-
-            let descriptor_write_sets = [vk::WriteDescriptorSet {
-                dst_set: descritptor_set,
-                dst_binding: 0,
-                dst_array_element: 0,
-                descriptor_count: descriptor_buffer_info.len() as u32,
-                descriptor_type: vk::DescriptorType::UNIFORM_BUFFER,
-                p_image_info: ptr::null(),
-                p_buffer_info: descriptor_buffer_info.as_ptr(),
-                p_texel_buffer_view: ptr::null(),
-                ..Default::default()
-            }];
-
-            unsafe {
-                device.update_descriptor_sets(&descriptor_write_sets, &[]);
-            }
-        }
-
-        descriptor_sets
     }
 
     pub fn update_camera(&mut self, camera: &Camera) {
@@ -2160,47 +2203,6 @@ impl Vulkan {
         }
 
         command_buffers
-    }
-
-    fn create_sync_objects(device: &ash::Device) -> SyncObjects {
-        let mut sync_objects = SyncObjects {
-            image_available_semaphores: vec![],
-            render_finished_semaphores: vec![],
-            inflight_fences: vec![],
-        };
-
-        let semaphore_create_info = vk::SemaphoreCreateInfo {
-            ..Default::default()
-        };
-
-        let fence_create_info = vk::FenceCreateInfo {
-            flags: vk::FenceCreateFlags::SIGNALED,
-            ..Default::default()
-        };
-
-        for _ in 0..MAX_FRAMES_IN_FLIGHT {
-            unsafe {
-                let image_available_semaphore = device
-                    .create_semaphore(&semaphore_create_info, None)
-                    .expect("Failed to create Semaphore Object!");
-                let render_finished_semaphore = device
-                    .create_semaphore(&semaphore_create_info, None)
-                    .expect("Failed to create Semaphore Object!");
-                let inflight_fence = device
-                    .create_fence(&fence_create_info, None)
-                    .expect("Failed to create Fence Object!");
-
-                sync_objects
-                    .image_available_semaphores
-                    .push(image_available_semaphore);
-                sync_objects
-                    .render_finished_semaphores
-                    .push(render_finished_semaphore);
-                sync_objects.inflight_fences.push(inflight_fence);
-            }
-        }
-
-        sync_objects
     }
 
     pub fn draw_frame(&mut self) {
