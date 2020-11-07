fc457b9666988f5cd5edcba0925341af45819a75
Jacek Bilski <jacek@bilski.tech>
2020-11-07 09:23:29 +0100

    VulkanCore gets its own file.

--- src/vulkan/mod.rs
+++ src/vulkan/mod.rs
@@ -1,11 +1,7 @@
-use std::collections::HashSet;
-use std::ffi::{CStr, CString};
-use std::os::raw::{c_char, c_void};
+use std::ffi::CString;
 use std::ptr;
 
-use ash::extensions::ext::DebugUtils;
-use ash::extensions::khr::{Surface, WaylandSurface, XlibSurface};
-use ash::version::{DeviceV1_0, EntryV1_0, InstanceV1_0};
+use ash::version::{DeviceV1_0, InstanceV1_0};
 use ash::vk;
 use cgmath::{Matrix4, Point3};
 use memoffset::offset_of;
@@ -14,10 +10,9 @@ use crate::fs::read_shader_code;
 use crate::mesh::{InstanceData, Mesh};
 use crate::scene::camera::Camera;
 use crate::scene::lights::{Light, Lights};
+use crate::vulkan::core::VulkanCore;
 
-const APPLICATION_VERSION: u32 = vk::make_version(0, 1, 0);
-const ENGINE_VERSION: u32 = vk::make_version(0, 1, 0);
-const VULKAN_API_VERSION: u32 = vk::make_version(1, 2, 154);
+mod core;
 
 const MAX_FRAMES_IN_FLIGHT: usize = 2;
 const CAMERA_UBO_INDEX: usize = 0;
@@ -29,7 +24,7 @@ struct SyncObjects {
     inflight_fences: Vec<vk::Fence>,
 }
 
-struct QueueFamilyIndices {
+pub struct QueueFamilyIndices {
     graphics_family: Option<u32>,
     compute_family: Option<u32>,
     transfer_family: Option<u32>,
@@ -53,13 +48,13 @@ impl QueueFamilyIndices {
     }
 }
 
-struct SurfaceComposite {
+pub struct SurfaceComposite {
     loader: ash::extensions::khr::Surface,
     surface: vk::SurfaceKHR,
 }
 
 #[derive(Clone)]
-struct SwapChainComposite {
+pub struct SwapChainComposite {
     loader: ash::extensions::khr::Swapchain,
     swapchain: vk::SwapchainKHR,
     images: Vec<vk::Image>,
@@ -190,384 +185,6 @@ impl From<&Lights> for LightsUBO {
                 LightUBO::from(lights.lights[0]),
                 LightUBO::from(lights.lights[1]),
             ],
-        }
-    }
-}
-
-struct VulkanCore {
-    _entry: ash::Entry,
-    instance: ash::Instance,
-
-    debug_utils_loader: ash::extensions::ext::DebugUtils,
-    debug_messenger: vk::DebugUtilsMessengerEXT,
-
-    physical_device: vk::PhysicalDevice,
-    physical_device_memory_properties: vk::PhysicalDeviceMemoryProperties,
-    device: ash::Device,
-
-    queue_family: QueueFamilyIndices,
-    graphics_queue: vk::Queue,
-    present_queue: vk::Queue,
-    transfer_queue: vk::Queue,
-}
-
-impl VulkanCore {
-    fn new(window: &winit::window::Window, application_name: &str) -> (Self, SurfaceComposite) {
-        let entry = ash::Entry::new().unwrap();
-        let instance = VulkanCore::create_instance(&entry, application_name);
-        let (debug_utils_loader, debug_messenger) =
-            VulkanCore::setup_debug_utils(&entry, &instance);
-        let surface_composite = VulkanCore::create_surface(&entry, &instance, &window);
-        let physical_device = VulkanCore::pick_physical_device(&instance, &surface_composite);
-        let physical_device_memory_properties =
-            unsafe { instance.get_physical_device_memory_properties(physical_device) };
-        let (device, queue_family) =
-            VulkanCore::create_logical_device(&instance, physical_device, &surface_composite);
-        let graphics_queue =
-            unsafe { device.get_device_queue(queue_family.graphics_family.unwrap(), 0) };
-        let present_queue =
-            unsafe { device.get_device_queue(queue_family.present_family.unwrap(), 0) };
-        let transfer_queue =
-            unsafe { device.get_device_queue(queue_family.transfer_family.unwrap(), 0) };
-        (
-            VulkanCore {
-                _entry: entry,
-                instance,
-
-                debug_utils_loader,
-                debug_messenger,
-
-                physical_device,
-                physical_device_memory_properties,
-
-                device,
-                queue_family,
-                graphics_queue,
-                present_queue,
-                transfer_queue,
-            },
-            surface_composite,
-        )
-    }
-
-    fn create_instance(entry: &ash::Entry, application_name: &str) -> ash::Instance {
-        let app_name = CString::new(application_name).unwrap();
-        let engine_name = CString::new("Vulkan Engine").unwrap();
-        let app_info = vk::ApplicationInfo {
-            p_application_name: app_name.as_ptr(),
-            application_version: APPLICATION_VERSION,
-            p_engine_name: engine_name.as_ptr(),
-            engine_version: ENGINE_VERSION,
-            api_version: VULKAN_API_VERSION,
-            ..Default::default()
-        };
-
-        let enabled_layer_raw_names: Vec<CString> = required_layer_names()
-            .iter()
-            .map(|name| CString::new(*name).unwrap())
-            .collect();
-        let enabled_layer_names: Vec<*const c_char> = enabled_layer_raw_names
-            .iter()
-            .map(|name| name.as_ptr())
-            .collect();
-
-        let enabled_extension_raw_names: Vec<CString> = required_extension_names()
-            .iter()
-            .map(|layer_name| CString::new(*layer_name).unwrap())
-            .collect();
-        let enabled_extension_names: Vec<*const c_char> = enabled_extension_raw_names
-            .iter()
-            .map(|layer_name| layer_name.as_ptr())
-            .collect();
-
-        let debug_utils_messenger_ci = VulkanCore::build_messenger_create_info();
-
-        let create_info = vk::InstanceCreateInfo {
-            p_application_info: &app_info,
-            p_next: &debug_utils_messenger_ci as *const vk::DebugUtilsMessengerCreateInfoEXT
-                as *const c_void,
-            enabled_layer_count: enabled_layer_names.len() as u32,
-            pp_enabled_layer_names: enabled_layer_names.as_ptr(),
-            enabled_extension_count: enabled_extension_names.len() as u32,
-            pp_enabled_extension_names: enabled_extension_names.as_ptr(),
-            ..Default::default()
-        };
-
-        let instance: ash::Instance = unsafe {
-            entry
-                .create_instance(&create_info, None)
-                .expect("Failed to create instance!")
-        };
-
-        instance
-    }
-
-    fn setup_debug_utils(
-        entry: &ash::Entry,
-        instance: &ash::Instance,
-    ) -> (ash::extensions::ext::DebugUtils, vk::DebugUtilsMessengerEXT) {
-        let debug_utils_loader = ash::extensions::ext::DebugUtils::new(entry, instance);
-
-        let messenger_ci = VulkanCore::build_messenger_create_info();
-
-        let utils_messenger = unsafe {
-            debug_utils_loader
-                .create_debug_utils_messenger(&messenger_ci, None)
-                .expect("Debug Utils Callback")
-        };
-
-        (debug_utils_loader, utils_messenger)
-    }
-
-    fn build_messenger_create_info() -> vk::DebugUtilsMessengerCreateInfoEXT {
-        vk::DebugUtilsMessengerCreateInfoEXT {
-            message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
-                // | vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
-                // | vk::DebugUtilsMessageSeverityFlagsEXT::INFO
-                | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
-            message_type: vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
-                | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE
-                | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION,
-            pfn_user_callback: Some(vulkan_debug_utils_callback),
-            ..Default::default()
-        }
-    }
-
-    #[cfg(all(unix, not(target_os = "android"), not(target_os = "macos")))]
-    fn create_surface(
-        entry: &ash::Entry,
-        instance: &ash::Instance,
-        window: &winit::window::Window,
-    ) -> SurfaceComposite {
-        let surface = unsafe {
-            use winit::platform::unix::WindowExtUnix;
-
-            if window.wayland_surface() != None {
-                println!("Using Wayland");
-                let wayland_surface = window.wayland_surface().unwrap();
-                let wayland_display = window.wayland_display().unwrap();
-                let wayland_create_info = vk::WaylandSurfaceCreateInfoKHR {
-                    surface: wayland_surface,
-                    display: wayland_display,
-                    ..Default::default()
-                };
-                let wayland_surface_loader = WaylandSurface::new(entry, instance);
-                wayland_surface_loader
-                    .create_wayland_surface(&wayland_create_info, None)
-                    .expect("Failed to create surface.")
-            } else {
-                println!("Using X11");
-                let x11_window = window.xlib_window().unwrap();
-                let x11_display = window.xlib_display().unwrap();
-                let x11_create_info = vk::XlibSurfaceCreateInfoKHR {
-                    window: x11_window,
-                    dpy: x11_display as *mut vk::Display,
-                    ..Default::default()
-                };
-                let xlib_surface_loader = XlibSurface::new(entry, instance);
-                xlib_surface_loader
-                    .create_xlib_surface(&x11_create_info, None)
-                    .expect("Failed to create surface.")
-            }
-        };
-        let surface_loader = ash::extensions::khr::Surface::new(entry, instance);
-
-        SurfaceComposite {
-            loader: surface_loader,
-            surface,
-        }
-    }
-
-    fn pick_physical_device(
-        instance: &ash::Instance,
-        surface_composite: &SurfaceComposite,
-    ) -> vk::PhysicalDevice {
-        let physical_devices: Vec<vk::PhysicalDevice> = unsafe {
-            instance
-                .enumerate_physical_devices()
-                .expect("Failed to enumerate Physical Devices!")
-        };
-
-        let result = physical_devices.iter().find(|physical_device| {
-            VulkanCore::is_physical_device_suitable(instance, **physical_device, &surface_composite)
-        });
-
-        match result {
-            None => panic!("Failed to find a suitable GPU!"),
-            Some(physical_device) => *physical_device,
-        }
-    }
-
-    fn is_physical_device_suitable(
-        instance: &ash::Instance,
-        physical_device: vk::PhysicalDevice,
-        surface_composite: &SurfaceComposite,
-    ) -> bool {
-        let indices = VulkanCore::find_queue_family(instance, physical_device, &surface_composite);
-        let is_queue_family_supported = indices.is_complete();
-
-        let is_device_extension_supported =
-            VulkanCore::check_device_extension_support(instance, physical_device);
-
-        let is_swapchain_supported = if is_device_extension_supported {
-            let swapchain_support =
-                VulkanGraphicsSetup::find_swapchain_support(physical_device, surface_composite);
-            !swapchain_support.formats.is_empty() && !swapchain_support.present_modes.is_empty()
-        } else {
-            false
-        };
-
-        return is_queue_family_supported
-            && is_device_extension_supported
-            && is_swapchain_supported;
-    }
-
-    fn find_queue_family(
-        instance: &ash::Instance,
-        physical_device: vk::PhysicalDevice,
-        surface_composite: &SurfaceComposite,
-    ) -> QueueFamilyIndices {
-        let queue_families =
-            unsafe { instance.get_physical_device_queue_family_properties(physical_device) };
-
-        let mut queue_family_indices = QueueFamilyIndices::new();
-
-        let mut index: u32 = 0;
-        for queue_family in queue_families.iter() {
-            if queue_family.queue_count > 0 {
-                if queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
-                    queue_family_indices.graphics_family = Some(index);
-                    queue_family_indices.transfer_family = Some(index);
-                }
-
-                if queue_family.queue_flags.contains(vk::QueueFlags::COMPUTE) {
-                    queue_family_indices.compute_family = Some(index);
-                    queue_family_indices.transfer_family = Some(index);
-                }
-
-                if queue_family.queue_flags.contains(vk::QueueFlags::TRANSFER) {
-                    queue_family_indices.transfer_family = Some(index);
-                }
-
-                let is_present_support = unsafe {
-                    surface_composite
-                        .loader
-                        .get_physical_device_surface_support(
-                            physical_device,
-                            index as u32,
-                            surface_composite.surface,
-                        )
-                        .unwrap()
-                };
-                if is_present_support {
-                    queue_family_indices.present_family = Some(index);
-                }
-            }
-
-            if queue_family_indices.is_complete() {
-                break;
-            }
-
-            index += 1;
-        }
-
-        queue_family_indices
-    }
-
-    fn check_device_extension_support(
-        instance: &ash::Instance,
-        physical_device: vk::PhysicalDevice,
-    ) -> bool {
-        let available_extensions = unsafe {
-            instance
-                .enumerate_device_extension_properties(physical_device)
-                .expect("Failed to get device extension properties.")
-        };
-
-        let mut available_extension_names = vec![];
-
-        for extension in available_extensions.iter() {
-            let extension_name = unsafe { CStr::from_ptr(extension.extension_name.as_ptr()) }
-                .to_str()
-                .unwrap()
-                .to_owned();
-            available_extension_names.push(extension_name);
-        }
-
-        let mut required_extensions = HashSet::new();
-        for extension in required_device_extensions().iter() {
-            required_extensions.insert(extension.to_string());
-        }
-
-        for extension_name in available_extension_names.iter() {
-            required_extensions.remove(extension_name);
-        }
-
-        return required_extensions.is_empty();
-    }
-
-    fn create_logical_device(
-        instance: &ash::Instance,
-        physical_device: vk::PhysicalDevice,
-        surface_composite: &SurfaceComposite,
-    ) -> (ash::Device, QueueFamilyIndices) {
-        let indices = VulkanCore::find_queue_family(instance, physical_device, surface_composite);
-
-        let queue_priorities: [f32; 1] = [1.0];
-        let queue_create_infos = [vk::DeviceQueueCreateInfo {
-            queue_family_index: indices.graphics_family.unwrap(),
-            queue_count: queue_priorities.len() as u32,
-            p_queue_priorities: queue_priorities.as_ptr(),
-            ..Default::default()
-        }];
-
-        let physical_device_features = vk::PhysicalDeviceFeatures {
-            ..Default::default()
-        };
-
-        let enabled_layer_raw_names: Vec<CString> = required_layer_names()
-            .iter()
-            .map(|name| CString::new(*name).unwrap())
-            .collect();
-        let enabled_layer_names: Vec<*const c_char> = enabled_layer_raw_names
-            .iter()
-            .map(|name| name.as_ptr())
-            .collect();
-        let enabled_extension_raw_names: Vec<CString> = required_device_extensions()
-            .iter()
-            .map(|name| CString::new(*name).unwrap())
-            .collect();
-        let enabled_extension_names: Vec<*const c_char> = enabled_extension_raw_names
-            .iter()
-            .map(|name| name.as_ptr())
-            .collect();
-
-        let device_create_info = vk::DeviceCreateInfo {
-            queue_create_info_count: queue_create_infos.len() as u32,
-            p_queue_create_infos: queue_create_infos.as_ptr(),
-            enabled_layer_count: enabled_layer_names.len() as u32,
-            pp_enabled_layer_names: enabled_layer_names.as_ptr(),
-            p_enabled_features: &physical_device_features,
-            enabled_extension_count: enabled_extension_names.len() as u32,
-            pp_enabled_extension_names: enabled_extension_names.as_ptr(),
-            ..Default::default()
-        };
-
-        let device: ash::Device = unsafe {
-            instance
-                .create_device(physical_device, &device_create_info, None)
-                .expect("Failed to create logical Device!")
-        };
-
-        (device, indices)
-    }
-
-    fn drop(&self) {
-        unsafe {
-            self.device.destroy_device(None);
-            self.debug_utils_loader
-                .destroy_debug_utils_messenger(self.debug_messenger, None);
-            self.instance.destroy_instance(None);
         }
     }
 }
@@ -2352,47 +1969,4 @@ impl Drop for Vulkan {
         self.graphics_setup.drop(&device);
         self.core.drop();
     }
-}
-
-/// the callback function used in Debug Utils.
-unsafe extern "system" fn vulkan_debug_utils_callback(
-    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
-    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
-    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
-    _p_user_data: *mut c_void,
-) -> vk::Bool32 {
-    let severity = match message_severity {
-        vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE => "[Verbose]",
-        vk::DebugUtilsMessageSeverityFlagsEXT::WARNING => "[Warning]",
-        vk::DebugUtilsMessageSeverityFlagsEXT::ERROR => "[Error]",
-        vk::DebugUtilsMessageSeverityFlagsEXT::INFO => "[Info]",
-        _ => "[Unknown]",
-    };
-    let types = match message_type {
-        vk::DebugUtilsMessageTypeFlagsEXT::GENERAL => "[General]",
-        vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE => "[Performance]",
-        vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION => "[Validation]",
-        _ => "[Unknown]",
-    };
-    let message = CStr::from_ptr((*p_callback_data).p_message);
-    println!("[Debug]{}{}{:?}", severity, types, message);
-
-    vk::FALSE
-}
-
-fn required_layer_names() -> Vec<&'static str> {
-    vec!["VK_LAYER_KHRONOS_validation"]
-}
-
-fn required_extension_names() -> Vec<&'static str> {
-    vec![
-        Surface::name().to_str().unwrap(),
-        XlibSurface::name().to_str().unwrap(),
-        WaylandSurface::name().to_str().unwrap(),
-        DebugUtils::name().to_str().unwrap(),
-    ]
-}
-
-fn required_device_extensions() -> Vec<&'static str> {
-    vec![ash::extensions::khr::Swapchain::name().to_str().unwrap()]
 }
