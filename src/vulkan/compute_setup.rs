c86dc0a8ee39d8ae7d982395905aecdb61589c7b
Jacek Bilski <jacek@bilski.tech>
2020-12-22 21:22:15 +0100

    Snowflakes are rotating less "nervously".

--- src/vulkan/compute_setup.rs
+++ src/vulkan/compute_setup.rs
@@ -73,7 +73,7 @@ impl VulkanComputeSetup {
         descriptor_set_layout: vk::DescriptorSetLayout,
     ) -> (vk::Pipeline, vk::PipelineLayout) {
         let comp_shader_module =
-            core.create_shader_module(include_bytes!("../../target/shaders/simple.comp.spv"));
+            core.create_shader_module(include_bytes!("../../target/shaders/snow.comp.spv"));
 
         let main_function_name = CString::new("main").unwrap(); // the beginning function name in shader code.
 
