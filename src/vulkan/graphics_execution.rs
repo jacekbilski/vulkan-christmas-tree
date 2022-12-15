88d833b109c5df23e95e100f3a4dbae44a930f84
Jacek Bilski <jacek@bilski.tech>
2022-12-16 00:07:49 +0100

    Adding VulkanGraphicsExecution.copy_buffer_to_image().

--- src/vulkan/graphics_execution.rs
+++ src/vulkan/graphics_execution.rs
@@ -887,6 +887,50 @@ impl VulkanGraphicsExecution {
             .end_one_time_commands(command_pool, &command_buffers, command_buffer);
     }
 
+    fn copy_buffer_to_image(
+        &self,
+        command_pool: vk::CommandPool,
+        buffer: vk::Buffer,
+        image: vk::Image,
+        width: u32,
+        height: u32,
+    ) {
+        let (command_buffers, command_buffer) = self.core.begin_one_time_commands(command_pool);
+
+        let region = vk::BufferImageCopy {
+            buffer_offset: 0,
+            buffer_row_length: 0,
+            buffer_image_height: 0,
+            image_subresource: vk::ImageSubresourceLayers {
+                aspect_mask: vk::ImageAspectFlags::COLOR,
+                mip_level: 0,
+                base_array_layer: 0,
+                layer_count: 1,
+                ..Default::default()
+            },
+            image_offset: vk::Offset3D { x: 0, y: 0, z: 0 },
+            image_extent: vk::Extent3D {
+                width,
+                height,
+                depth: 1,
+            },
+            ..Default::default()
+        };
+
+        unsafe {
+            self.core.device.cmd_copy_buffer_to_image(
+                command_buffer,
+                buffer,
+                image,
+                vk::ImageLayout::TRANSFER_DST_OPTIMAL,
+                &[region],
+            );
+        }
+
+        self.core
+            .end_one_time_commands(command_pool, &command_buffers, command_buffer);
+    }
+
     pub(crate) fn drop(&mut self) {
         unsafe {
             let device = &self.core.device;
