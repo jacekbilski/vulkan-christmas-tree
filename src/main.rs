8910422ff99200ab383025fd552928f324a8ea13
Jacek Bilski <jacek@bilski.tech>
2024-12-05 21:55:58 +0100

    Upgrading winit to version 0.29.15, part 1/?.

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,12 @@ use std::f32::consts::{FRAC_PI_8, TAU};
 use std::thread;
 use std::time::{Duration, Instant};
 
+use vulkan::Vulkan;
 use winit::dpi::{PhysicalPosition, PhysicalSize};
 use winit::event::ElementState::Pressed;
-use winit::event::{
-    Event, KeyboardInput, MouseButton, MouseScrollDelta, VirtualKeyCode, WindowEvent,
-};
-use winit::event_loop::{ControlFlow, EventLoop};
-
-use vulkan::Vulkan;
+use winit::event::{Event, KeyEvent, MouseButton, MouseScrollDelta, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::keyboard::{KeyCode, PhysicalKey};
 
 use crate::fps_calculator::FpsCalculator;
 use crate::scene::Scene;
@@ -31,7 +29,7 @@ const MAX_FPS: u8 = 60;
 const APPLICATION_NAME: &'static str = "Vulkan Christmas Tree";
 
 fn main() {
-    let event_loop = EventLoop::new();
+    let event_loop = EventLoop::new().unwrap();
     let window = init_window(&event_loop);
     let mut vulkan = Vulkan::new(&window, APPLICATION_NAME);
     let scene = Scene::setup(&mut vulkan, &window);
@@ -52,7 +50,7 @@ fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window {
         .map(|monitor| monitor.size())
         .map(|size| PhysicalSize::new(size.width / 2, size.height / 2))
         .unwrap_or(PhysicalSize::new(1600, 900));
-    window.set_inner_size(screen_size);
+    window.request_inner_size(screen_size).unwrap();
 
     window
 }
@@ -68,52 +66,52 @@ fn main_loop(
     let mut mouse_rotating = false;
     let mut last_cursor_position: PhysicalPosition<f64> = PhysicalPosition::new(0.0, 0.0);
     let desired_frame_duration = Duration::from_secs_f32(1.0 / MAX_FPS as f32);
-    event_loop.run(move |event, _, control_flow| match event {
+    event_loop.run(move |event, elwt| match event {
         Event::WindowEvent {
             event: WindowEvent::CloseRequested,
             ..
         } => {
             vulkan.wait_device_idle();
-            *control_flow = ControlFlow::Exit;
+            elwt.exit();
         }
         Event::WindowEvent {
             event:
                 WindowEvent::KeyboardInput {
-                    input:
-                        KeyboardInput {
-                            virtual_keycode: Some(virtual_code),
+                    event:
+                        KeyEvent {
+                            physical_key: PhysicalKey::Code(code),
                             state: Pressed,
                             ..
                         },
                     ..
                 },
             ..
-        } => match virtual_code {
-            VirtualKeyCode::Escape => {
+        } => match code {
+            KeyCode::Escape => {
                 vulkan.wait_device_idle();
-                *control_flow = ControlFlow::Exit;
+                elwt.exit();
             }
-            VirtualKeyCode::Up => {
+            KeyCode::ArrowUp => {
                 autorotate = false;
                 let angle_change = FRAC_PI_8 / 4.;
                 scene.rotate_camera_vertically(angle_change, &mut vulkan);
             }
-            VirtualKeyCode::Down => {
+            KeyCode::ArrowDown => {
                 autorotate = false;
                 let angle_change = FRAC_PI_8 / 4.;
                 scene.rotate_camera_vertically(-angle_change, &mut vulkan);
             }
-            VirtualKeyCode::Left => {
+            KeyCode::ArrowLeft => {
                 autorotate = false;
                 let angle_change = FRAC_PI_8 / 4.;
                 scene.rotate_camera_horizontally(-angle_change, &mut vulkan);
             }
-            VirtualKeyCode::Right => {
+            KeyCode::ArrowRight => {
                 autorotate = false;
                 let angle_change = FRAC_PI_8 / 4.;
                 scene.rotate_camera_horizontally(angle_change, &mut vulkan);
             }
-            VirtualKeyCode::R => {
+            KeyCode::KeyR => {
                 autorotate = !autorotate;
             }
             _ => (),
@@ -161,10 +159,10 @@ fn main_loop(
             scene.framebuffer_resized(new_size, &mut vulkan);
             vulkan.framebuffer_resized(new_size.width, new_size.height);
         }
-        Event::MainEventsCleared => {
+        Event::AboutToWait => {
             window.request_redraw();
         }
-        Event::RedrawRequested(_window_id) => {
+        WindowEvent::RedrawRequested => {
             let frame_start = Instant::now();
             fps_calculator.tick();
             let last_frame_time_secs = fps_calculator.last_frame_time_secs();
@@ -181,7 +179,7 @@ fn main_loop(
                 thread::sleep(desired_frame_duration - actual_frame_duration);
             }
         }
-        Event::LoopDestroyed => {
+        Event::LoopExiting => {
             vulkan.wait_device_idle();
         }
         _ => (),
